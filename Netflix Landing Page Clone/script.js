988ed4c952ebd251e0a9def3b8cc2d835ff3e7da
Adrian Bogdan <90060321+Adrianbgd@users.noreply.github.com>
2021-10-04 03:08:26 -0700

    Delete Netflix Landing Page Clone directory

--- Netflix Landing Page Clone/script.js
+++
@@ -1,7 +0,0 @@
-    const togglevideo = () => {
-        const Trailer = document.querySelector('.trailer');
-        const video = document.querySelector('video');
-        Trailer.classList.toggle('active');
-        video.currentTime = 0;
-        video.pause();
-    }
